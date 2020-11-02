605c09f8c8f529a5af778d5efd7564a24319d630
Guilherme <aliniribeiroo@gmail.com>
2020-11-02 19:19:42 -0300

    Ajustes vars

--- pokedex/views/home.py
+++ pokedex/views/home.py
@@ -6,4 +6,4 @@ home_blueprint = Blueprint('', __name__, template_folder='templates')
 @home_blueprint.route('/')
 def index():
     logged_in = session.get('usuario_logado', None) is not None
-    return render_template('home.py.html', login=logged_in)
+    return render_template('home.html', login=logged_in)
